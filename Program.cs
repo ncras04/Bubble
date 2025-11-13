b3eb927119e988c42eb0cedae8a04a86c201b7f6
Student <student@sae.de>
2025-11-13 16:17:36 +0100

    ja lol

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Bubble blub = new Bubble();
+            blub.Bubb();
         }
     }
 }
